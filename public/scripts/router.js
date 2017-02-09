7271f51218ff1c30ceb2bfd36ecb213a52c4e090
acpbm <acpbm0@gmail.com>
2017-02-09 18:15:13 -0500

    Set up ui router with state index, and state signup

---
+++ public/scripts/router.js
@@ -0,0 +1,17 @@
+angular.module('memeApp', ['ui-router'])
+.config(MemeRouter);
+
+function MemeRouter($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('index', {
+    url: '/',
+    templateUrl: '/partials/home.html'
+  })
+  .state('signup', {
+    url: '/signup',
+    templateUrl: '/partials/signup.html'
+  })
+}
