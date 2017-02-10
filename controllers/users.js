ece8db9dc164df1cc65c6517bfd7bb9209b853ca
acpbm <acpbm0@gmail.com>
2017-02-09 19:21:55 -0500

    Add post('/') route. This post request adds new user to DB on signup

--- controllers/users.js
+++ controllers/users.js
@@ -4,8 +4,23 @@ var Meme = require('../models/meme.js');
 var User = require('../models/user.js');
 var authHelpers = require('../helpers/auth.js');
 
-router.post('/signup', function(req, res){
+router.post('/', authHelpers.createSecure, function(req, res){
+
   console.log("backend post route");
+
+  var user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password_digest: res.hashedPassword
+  });
+
+  user.save(function(err, user){
+    if(err){ console.log(err); }
+    console.log(user);
+    res.json({status: 201, message: 'user created'});
+  });
+
 });
 
 
