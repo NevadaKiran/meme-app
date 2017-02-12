77cf7184cb53e361ddcf5c896e6ab6db395c4b09
acpbm <acpbm0@gmail.com>
2017-02-11 22:52:17 -0500

    Add empty get route

--- controllers/memes.js
+++ controllers/memes.js
@@ -4,6 +4,8 @@ var Meme = require('../models/meme.js');
 var User = require('../models/user.js');
 var authHelpers = require('../helpers/auth.js');
 
-
+// router.get('/', function(req, res) {
+//
+// });
 
 module.exports = router;
